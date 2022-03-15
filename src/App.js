36e74aedecad0b7774c9da0c3ae9277b3965b77c
webdevwdc <hello@webskitters.com>
2022-03-15 16:40:02 +0530

    cccss

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import logo from "./logo.svg";
-import "./App.scss";
+
+
 import { Route, Routes } from "react-router-dom";
-import Header from "./ShareModules/Layout/Header/Header";
-import Footer from "./ShareModules/Layout/Footer/Footer";
-import Index from "./Components/Home/Index";
-import PrivateOutlet from "./ShareModules/RouterWraper/PrivateOutlet";
+// import Header from "./ShareModules/Layout/Header/Header";
+// import Footer from "./ShareModules/Layout/Footer/Footer";
+// import Index from "./Components/Home/Index";
+// import PrivateOutlet from "./ShareModules/RouterWraper/PrivateOutlet";
 
 function App() {
   return (
     <div className="App">
     <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
+    
       <p>
         Edit <code>src/App.js</code> and save to reload.
       </p>
