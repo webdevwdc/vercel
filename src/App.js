9780f907522a086326b26cc8756d30c7304ec0ac
Subham Sarkar <subham.sarkar@webskitters.com>
2022-03-15 13:58:08 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import logo from "./logo.svg";
+import "./App.scss";
+import { Route, Routes } from "react-router-dom";
+import Header from "./ShareModules/Layout/Header/Header";
+import Footer from "./ShareModules/Layout/Footer/Footer";
+import Index from "./Components/Home/Index";
+import PrivateOutlet from "./ShareModules/RouterWraper/PrivateOutlet";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Routes>
+        {/**************  Start public Route *********************/}
+        <Route path="/" element={<Index />} />
+
+        {/***************  End public Route  **********************/}
+
+        {/**************** Start Private Route *********************/}
+        {/* <Route path="/*" element={<PrivateOutlet />}>
+              <Route path="demo" element={<Demo />} />
+            </Route> */}
+        {/****************** End Private Route  *********************/}
+      </Routes>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
